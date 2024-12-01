e03ea3e83879fc1575f9752408d214561824b4df
GuiMM <guilherme.macedomoreira@gmail.com>
2024-12-01 12:40:16 -0300

    WIP: criacao DBatlas

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "project_mongo_id" {
+  description = "Project Id mongo atlas."
+  type        = string
+  default     = "6747aac9c76de323d87944fc"
+}
+variable "storage_type" {
+  description = "type of the storage"
+  type        = string
+  default     = "gp3"
+}
